1bc03b7f198e0809779f97935a1aadab0e511323
OpenShift guest <xunfeizheng@mybihs.org>
2014-04-05 21:50:56 -0400

    new homework

--- homework/4-memory-game/Memory.js
+++ homework/4-memory-game/Memory.js
@@ -1,31 +1,44 @@
 $( document ).ready(function() {
     
     var clicks = 0;
-    var Match = " ";
-    var firstclick = " ";
-    var firstid = " ";
+    var Match = false;
+    var firstclick = "0";
+    var firstid = "0";
     $(".td").click(function(){
-        var boxes = $(this).attr("id"); 
-        $(this).html("<img id='pic-" + boxes + " 'src=\"" + pictures[boxes] + "\" height=\"80px\" width=\"80px\">");
-        alert("you click " + boxes );
-        var picId = "#pic-" + boxes;
+        var boxes = $(this).attr("id");
+        if(flipped[boxes] === true){}
+        else{
+         
+        $(this).html(pictures[boxes]);
+       // alert("you click " + boxes );
         flipped[boxes] = true;
         clicks = clicks +1;
         if(clicks == 1){
-            firstclick = $(picId).attr("src");
-            firstid = "#" + $(this).attr("id");
+            firstclick = $(this).children();
+            firstid = boxes;
         }
         else if(clicks == 2){
-            var secondclick = $(picId).attr("src");
-            var secondid = "#" + $(this).attr("id");
-            if(firstclick == secondclick){
-            Match = true;
+            var secondclick = $(this).children();
+            var secondid = boxes;
+            if(firstclick.attr("src") == secondclick.attr("src")){
+                Match = true;
+                Score();
+                match();
             }
             else{
-                $(firstid).text('?');
-                $(secondid).text('?');
+                Match = false;
+                match();
+                setTimeout(function (){
+                $("#" + firstid).text('?');
+                flipped[firstid] = false;
+                $("#" + secondid).text('?');
+                flipped[secondid] = false;
+                Score();
+                }, 500);
             }
             clicks = 0;
+            flip();
+            }
         }
     });
     
@@ -33,47 +46,91 @@ $( document ).ready(function() {
         alert("Reset clicked!");
         $(".td").text('?');
         $("#score").text('0');
+        $("#alert").text(" ");
+        for(var i=0; i<16; i++){
+            flipped[i] = false;
+        }
+        
+        randompics(pictures);   
     });
     
-    var pictures = [ "http://www.intelligentspeculator.net/wp-content/uploads/2011/08/aapl1.jpg",
-        "http://www.thehulltruth.com/attachments/boating-forum/384422d1388078692-help-my-dad-name-his-boat-florida-citrus-grower-orange-slice-background.jpg",
-        "http://baby-recipes.com/wp-content/uploads/2014/02/banana.jpg",
-        "http://extreme-couponing-tips.com/wp-content/uploads/2013/06/extreme-couponing-tips-peach.jpg",
-        "http://img.wallpaperstock.net:81/yellow-pear-wallpapers_13081_1600x1200.jpg",
-        "http://www.nation.com.pk/print_images/670/2013-08-30/first-mango-consignment-dispatched-to-australia-1377807826-9127.jpg",
-        "http://www.patelbrothersirving.com/wp-content/uploads/2010/06/water-melomd.jpg",
-        "http://argotea.com/blog/wp-content/uploads/2013/09/strawberries.jpg",
-        "http://www.intelligentspeculator.net/wp-content/uploads/2011/08/aapl1.jpg",
-        "http://www.thehulltruth.com/attachments/boating-forum/384422d1388078692-help-my-dad-name-his-boat-florida-citrus-grower-orange-slice-background.jpg",
-        "http://baby-recipes.com/wp-content/uploads/2014/02/banana.jpg",
-        "http://extreme-couponing-tips.com/wp-content/uploads/2013/06/extreme-couponing-tips-peach.jpg",
-        "http://img.wallpaperstock.net:81/yellow-pear-wallpapers_13081_1600x1200.jpg",
-        "http://www.nation.com.pk/print_images/670/2013-08-30/first-mango-consignment-dispatched-to-australia-1377807826-9127.jpg",
-        "http://www.patelbrothersirving.com/wp-content/uploads/2010/06/water-melomd.jpg",
-        "http://argotea.com/blog/wp-content/uploads/2013/09/strawberries.jpg"
+    var pictures = [ "<img src=\"http://www.intelligentspeculator.net/wp-content/uploads/2011/08/aapl1.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://www.thehulltruth.com/attachments/boating-forum/384422d1388078692-help-my-dad-name-his-boat-florida-citrus-grower-orange-slice-background.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://baby-recipes.com/wp-content/uploads/2014/02/banana.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://extreme-couponing-tips.com/wp-content/uploads/2013/06/extreme-couponing-tips-peach.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://img.wallpaperstock.net:81/yellow-pear-wallpapers_13081_1600x1200.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://blog.goodmeasuremeals.com/wp-content/uploads/2013/08/1363010941_peaches_fruit_wallpaper.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://www.patelbrothersirving.com/wp-content/uploads/2010/06/water-melomd.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://argotea.com/blog/wp-content/uploads/2013/09/strawberries.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://www.intelligentspeculator.net/wp-content/uploads/2011/08/aapl1.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://www.thehulltruth.com/attachments/boating-forum/384422d1388078692-help-my-dad-name-his-boat-florida-citrus-grower-orange-slice-background.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://baby-recipes.com/wp-content/uploads/2014/02/banana.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://extreme-couponing-tips.com/wp-content/uploads/2013/06/extreme-couponing-tips-peach.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://img.wallpaperstock.net:81/yellow-pear-wallpapers_13081_1600x1200.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://blog.goodmeasuremeals.com/wp-content/uploads/2013/08/1363010941_peaches_fruit_wallpaper.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://www.patelbrothersirving.com/wp-content/uploads/2010/06/water-melomd.jpg\" height=\"80px\" width=\"80px\">",
+        "<img src=\"http://argotea.com/blog/wp-content/uploads/2013/09/strawberries.jpg\" height=\"80px\" width=\"80px\">"
     ];
     var flipped = [];
-
-    var flipover = 0; 
-    for(var i=0; i<16; i++){ 
-        if(flipped[i] === true){ 
+    for(var i=0; i<16; i++){
+        flipped[i] = false;
+    }
+    
+    function flip(){
+        var flipover = 0; 
+        for(var x=0; x<16; x++){ 
+        if(flipped[x] === true){ 
             flipover = flipover + 1; 
             }     
         if(flipover == 16){
             alert("Congratulation! You finish the game."); 
             }
-        }
+        } 
+    }
+    
     
     function Score(){
-        var score =  $("#score").val();
+        var score =  parseInt($("#score").text());
         if (Match === true){
-            score =score+1;
+            score = score+1;
         }
         else{
            if(score > 0){
-           score =score-1; 
+           score = score-1; 
           }
         }
         $("#score").text(score);
     }
+    
+    function match(){
+        if(Match === true){
+            $("#alert").text("Match");
+        }
+        else{
+            $("#alert").text("Unmatch");
+        }
+    }
+    
+    function randompics(array) {
+        var currentIndex = array.length
+        , temporaryValue
+        , randomIndex
+        ;
+
+    // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
 });
